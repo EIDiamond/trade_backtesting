43d2b6a87bdd8d3b02c65a242fd7b9f6dad9a8d6
EIDiamond <105827519+EIDiamond@usersnoreply.github.com>
2025-04-16 01:27:05 +0300

    Removed unused setting (lot_size)

--- configuration/settings.py
+++ configuration/settings.py
@@ -12,7 +12,6 @@ class StrategySettings:
     # All internal strategy settings are represented as dict. A strategy class have to parse it himself.
     # Here, we avoid any strong dependencies and obligations
     settings: dict = field(default_factory=dict)
-    lot_size: int = 1
     short_enabled_flag: bool = True
 
 
